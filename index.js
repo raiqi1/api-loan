a99475396679e123aa6c18fef953c2fef1891cdf
rayiqi <ramadifan769@gmail.com>
2024-11-02 07:04:11 +0700

    add cors

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors(
-  {
-    origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: ["https://vue-frontend-alpha.vercel.app", "http://localhost:5173"],
     credentials: true,
-  }
-));
+  })
+);
 
 // Routers
 app.use("/api/users", userRouter);
